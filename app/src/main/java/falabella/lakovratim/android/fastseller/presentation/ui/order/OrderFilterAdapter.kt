68520df07f57928006dd3d1c544d7f8996460017
Mauricio A Vargas O <mandvargas@falabella.cl>
2020-09-25 23:22:22 -0300

    sea agrega color verde a la seleccion

--- app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/order/OrderFilterAdapter.kt
+++ app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/order/OrderFilterAdapter.kt
@@ -1,12 +1,18 @@
 package falabella.lakovratim.android.fastseller.presentation.ui.order
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.view.forEach
 import androidx.recyclerview.widget.RecyclerView
+import falabella.lakovratim.android.fastseller.R
 import falabella.lakovratim.android.fastseller.databinding.AdapterFilterItemBinding
 import javax.inject.Inject
 
-class OrderFilterAdapter @Inject constructor() : RecyclerView.Adapter<OrderFilterAdapter.ViewHolder>() {
+class OrderFilterAdapter @Inject constructor() :
+    RecyclerView.Adapter<OrderFilterAdapter.ViewHolder>() {
 
     var items: List<String> = listOf()
 
@@ -22,12 +28,17 @@ class OrderFilterAdapter @Inject constructor() : RecyclerView.Adapter<OrderFilte
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val filter = items[position]
-        holder.filterName.text = filter.toString()
+        holder.filterName.text = filter
+
+        holder.filterItem.setOnClickListener {
+            holder.filterItem.setBackgroundResource(R.drawable.button_item_filter_green)
+        }
     }
 
     override fun getItemCount(): Int = items.size
 
     class ViewHolder(binding: AdapterFilterItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val filterName = binding.textFilter
+        val filterItem = binding.filterItem
     }
 }
