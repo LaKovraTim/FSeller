b12214484e37b4c8c7a92c5222c0f2dd6a998e9f
Mauricio A Vargas O <mandvargas@falabella.cl>
2020-09-26 03:54:40 -0300

    se agrega metodo para abrir waze

--- app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/taskdetail/TaskDetailFragment.kt
@@ -1,16 +1,32 @@
 package falabella.lakovratim.android.fastseller.presentation.ui.taskdetail
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import falabella.lakovratim.android.fastseller.databinding.FragmentTaskDetailBinding
 import falabella.lakovratim.android.fastseller.presentation.util.BaseFragment
 
-class TaskDetailFragment : BaseFragment<FragmentTaskDetailBinding>(){
+
+class TaskDetailFragment : BaseFragment<FragmentTaskDetailBinding>() {
 
     override fun setBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentTaskDetailBinding = FragmentTaskDetailBinding.inflate(inflater, container, false)
 
+
+    private fun openWaze() {
+        try {
+            val url = "https://waze.com/ul?q=Hawaii"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        } catch (ex: ActivityNotFoundException) {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"))
+            startActivity(intent)
+        }
+    }
 }
 
