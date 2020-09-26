42b6b3d748102be14e1f4b41dd5b2c130d85a886
Mauricio A Vargas O <mandvargas@falabella.cl>
2020-09-25 21:01:25 -0300

    se agrega buscador al diseño

--- app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/MainActivity.kt
+++ app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.WorkManager
+import falabella.lakovratim.android.fastseller.R
 import falabella.lakovratim.android.fastseller.data.remote.workRequest
 import falabella.lakovratim.android.fastseller.databinding.ActivityMainBinding
 
