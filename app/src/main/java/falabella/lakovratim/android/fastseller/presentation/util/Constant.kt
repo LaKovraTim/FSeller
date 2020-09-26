c281512e05323fb2e3c8527754377d38a2b143de
Andres Lobos <alobos@technisys.com>
2020-09-25 21:21:50 -0300

    fix login

--- app/src/main/java/falabella/lakovratim/android/fastseller/presentation/util/Constant.kt
+++ app/src/main/java/falabella/lakovratim/android/fastseller/presentation/util/Constant.kt
@@ -1,6 +1,6 @@
 package falabella.lakovratim.android.fastseller.presentation.util
 
 object Constant {
-    const val USERNAME_PATTERN = "^[a-z0-9_-]{6,30}$"
+    const val USERNAME_PATTERN = "^[a-z0-9_-]{3,30}$"
     const val USER_ARG = "user"
 }
