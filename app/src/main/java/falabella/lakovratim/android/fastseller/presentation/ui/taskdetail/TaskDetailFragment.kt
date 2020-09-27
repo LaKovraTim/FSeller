f7bcddd558926cdb91fa6410674272383fee1b4e
Mauricio A Vargas O <mandvargas@falabella.cl>
2020-09-27 00:24:56 -0300

    se valida entidad cashOnDelivery

--- app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/falabella/lakovratim/android/fastseller/presentation/ui/taskdetail/TaskDetailFragment.kt
@@ -17,6 +17,7 @@ import falabella.lakovratim.android.fastseller.presentation.appComponent
 import falabella.lakovratim.android.fastseller.presentation.ui.MainActivityViewModel
 import falabella.lakovratim.android.fastseller.presentation.util.BaseFragment
 import falabella.lakovratim.android.fastseller.presentation.util.OrderMenu
+import falabella.lakovratim.android.fastseller.presentation.util.extension.visible
 import javax.inject.Inject
 
 
@@ -54,10 +55,17 @@ class TaskDetailFragment : BaseFragment<FragmentTaskDetailBinding>() {
             binding.textRealizedByValue.text =
                 "${it.customer?.firstName} ${it.customer?.secondName}"
             binding.textCreationDate.text = it.creationDate!!
-           // binding.textAmount.text = it.
-            binding.textReceiverName.text = "${it.customer?.receiver?.firstName} ${it.customer?.receiver?.secondName}"
-            binding.textAddress.text ="${it.customer?.address?.street} ${it.customer?.address?.number} ${it.customer?.address?.comuna}"
+            binding.textAmount.text = "$${(it.total ?: "").toString()}"
+            binding.textReceiverName.text =
+                "${it.customer?.receiver?.firstName} ${it.customer?.receiver?.secondName}"
+            binding.textAddress.text =
+                "${it.customer?.address?.street} ${it.customer?.address?.number} ${it.customer?.address?.comuna}"
             binding.textReceiverDate.text = it.deliveryDate
+
+
+            if (it.cashOnDelivery) {
+                binding.cashOnDelivery.visible()
+            }
         }
     }
 
@@ -135,7 +143,8 @@ class TaskDetailFragment : BaseFragment<FragmentTaskDetailBinding>() {
 
             val intentWaze = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             intentWaze.setPackage("com.waze")
-            val uriGoogle = "google.navigation:q=" + viewModel.workOrder.value?.customer?.address?.location?.lat + "," + viewModel.workOrder.value?.customer?.address?.location?.lon
+            val uriGoogle =
+                "google.navigation:q=" + viewModel.workOrder.value?.customer?.address?.location?.lat + "," + viewModel.workOrder.value?.customer?.address?.location?.lon
 
             val intentGoogleNav = Intent(Intent.ACTION_VIEW, Uri.parse(uriGoogle))
             intentGoogleNav.setPackage("com.google.android.apps.maps")
@@ -152,15 +161,19 @@ class TaskDetailFragment : BaseFragment<FragmentTaskDetailBinding>() {
     }
 
 
-
-
-
     private fun callCustomer() {
         try {
-            val surf = Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+ viewModel.workOrder.value?.customer?.contact?.phone))
+            val surf = Intent(
+                Intent.ACTION_DIAL,
+                Uri.parse("tel:" + viewModel.workOrder.value?.customer?.contact?.phone)
+            )
             requireActivity().startActivity(surf)
         } catch (e: Exception) {
-            Toast.makeText(context, "No es posible obtener el número del cliente", Toast.LENGTH_SHORT)
+            Toast.makeText(
+                context,
+                "No es posible obtener el número del cliente",
+                Toast.LENGTH_SHORT
+            )
                 .show()
         }
 
